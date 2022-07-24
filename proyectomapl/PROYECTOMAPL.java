d58b39758d73d4f78687833c8a8544ff876df38b
Miguel PL <mig.parlop@gmail.com>
2022-07-24 11:31:05 -0500

    Inicio del proyecto sobre naves espaciales

---
+++ proyectomapl/PROYECTOMAPL.java
@@ -0,0 +1,11 @@
+
+package proyectomapl;
+
+public class PROYECTOMAPL {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+    }
+}
